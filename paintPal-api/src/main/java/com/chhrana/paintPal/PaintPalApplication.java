de8cfffb00822bebbe33e9eb1500babc856946b1
Chhavi Rana <chhavirana83@gmail.com>
2025-03-01 13:47:00 +0530

    initial commit

---
+++ paintPal-api/src/main/java/com/chhrana/paintPal/PaintPalApplication.java
@@ -0,0 +1,13 @@
+package com.chhrana.paintPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintPalApplication.class, args);
+	}
+
+}

6c1877795ae37830c99614b91a3bf71dcd307d4c
Chhavi Rana <chhavirana83@gmail.com>
2025-03-17 23:37:19 +0530

    implement register and activate account fn

---
+++ paintPal-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/services';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-activate-account',
+  templateUrl: './activate-account.component.html',
+  styleUrl: './activate-account.component.scss'
+})
+export class ActivateAccountComponent {
+  message = '';
+  isOkay = true;
+  submitted = false;
+  value: any;
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+  private confirmAccount(token: string) {
+    this.authService.confirm({
+      token
+    }).subscribe({
+      next: () => {
+        this.message = 'Your account has been successfully activated.\nNow you can proceed to login';
+        this.submitted = true;
+      },
+      error: () => {
+        this.message = 'Token has been expired or invalid';
+        this.submitted = true;
+        this.isOkay = false;
+      }
+    });
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  onCodeCompleted() {
+    this.confirmAccount(this.value);
+  }
+
+  // protected readonly skipUntil = skipUntil;
+}
---
+++ paintPal-ui/src/app/pages/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import { RegistrationRequest } from '../../services/models';
+import { AuthenticationService } from '../../services/services/authentication.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+  registerRequest: RegistrationRequest = {email: '', firstName: '', lastName: '', password: ''};
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService.register({
+      body: this.registerRequest
+    })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['activate-account']);
+        },
+        error: (err) => {
+          this.errorMsg = err.error.validationErrors;
+        }
+      });
+  }
+}
